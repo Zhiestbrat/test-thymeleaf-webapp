5984387693fa970cca85f684eff4f8100fc022dc
Zhiestbrat <fffcondakov@yandex.ru>
2022-10-18 22:32:38 +0500

    Initial commit

---
+++ src/main/java/com/example/testthymeleafwebapp/controller/StudentController.java
@@ -0,0 +1,2 @@
+package com.example.testthymeleafwebapp.controller;public class StudentController {
+}
---
+++ src/main/java/com/example/testthymeleafwebapp/dao/StudentRepository.java
@@ -0,0 +1,2 @@
+package com.example.testthymeleafwebapp.dao;public interface StudentRepository {
+}
